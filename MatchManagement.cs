1ca097babbb4ab8c827940d4bc9f5c50f3fdf5f0
Aakash-SI <aakash.natani@sportzinteractive.net>
2023-09-11 12:23:05 +0530

    Submission

---
+++ MatchManagement.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MatchManagement
+{
+    private List<MatchDetails> matches;
+
+    public MatchManagement()
+    {
+        matches = new List<MatchDetails>();
+    }
+
+    // Add Match Validation Method
+    private bool ValidateMatchDetails(MatchDetails match)
+    {
+        return true;
+    }
+
+    public void AddMatch(MatchDetails match)
+    {
+        if (ValidateMatchDetails(match))
+        {
+            matches.Add(match);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Match Details Because Match is Not Added");
+        }
+    }
+
+    // Implement Sorting Matches based on criteria and order
+    public void SortMatches(string criteria, bool ascending)
+    {
+        switch (criteria.ToLower())
+        {
+            case "date":
+                matches = ascending ? matches.OrderBy(m => m.MatchDateTime).ToList() : matches.OrderByDescending(m => m.MatchDateTime).ToList();
+                break;
+            case "sport":
+                matches = ascending ? matches.OrderBy(m => m.Sport).ToList() : matches.OrderByDescending(m => m.Sport).ToList();
+                break;
+            case "location":
+                matches = ascending ? matches.OrderBy(m => m.Location).ToList() : matches.OrderByDescending(m => m.Location).ToList();
+                break;
+            default:
+                Console.WriteLine("Invalid Sorting");
+                break;
+        }
+    }
+
+    // Implement Filtering Matches based on criteria
+    public List<MatchDetails> FilterMatches(string criteria, string value)
+    {
+        switch (criteria.ToLower())
+        {
+            case "sport":
+                return matches.Where(m => m.Sport.Equals(value, StringComparison.OrdinalIgnoreCase)).ToList();
+            case "location":
+                return matches.Where(m => m.Location.Equals(value, StringComparison.OrdinalIgnoreCase)).ToList();
+            case "date range":
+                DateTime startDate;
+                DateTime endDate;
+                if (DateTime.TryParse(value.Split('-')[0], out startDate) && DateTime.TryParse(value.Split('-')[1], out endDate))
+                {
+                    return matches.Where(m => m.MatchDateTime >= startDate && m.MatchDateTime <= endDate).ToList();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Use Format 'yyyy-mm-dd - yyyy-mm-dd'");
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid Filtering Criteria");
+                break;
+        }
+
+        return new List<MatchDetails>();
+    }
+
+    // Implement Statistics Calculation based on match scores
+    public void CalculateStatistics(string criteria)
+    {
+        switch (criteria.ToLower())
+        {
+            case "average score":
+                double homeAvg = matches.Average(m => m.HomeTeamScore);
+                double awayAvg = matches.Average(m => m.AwayTeamScore);
+                Console.WriteLine($"Average Score - Home: {homeAvg}, Away: {awayAvg}");
+                break;
+            case "highest score":
+                int highestHomeScore = matches.Max(m => m.HomeTeamScore);
+                int highestAwayScore = matches.Max(m => m.AwayTeamScore);
+                Console.WriteLine($"Highest Score - Home: {highestHomeScore}, Away: {highestAwayScore}");
+                break;
+            case "lowest score":
+                int lowestHomeScore = matches.Min(m => m.HomeTeamScore);
+                int lowestAwayScore = matches.Min(m => m.AwayTeamScore);
+                Console.WriteLine($"Lowest Score - Home: {lowestHomeScore}, Away: {lowestAwayScore}");
+                break;
+            default:
+                Console.WriteLine("Invalid");
+                break;
+        }
+    }
+
+    // Implement Search Matches based on keywords
+    public List<MatchDetails> SearchMatches(string keyword)
+    {
+        return matches.Where(m =>
+            m.Sport.Contains(keyword, StringComparison.OrdinalIgnoreCase) ||
+            m.HomeTeam.Contains(keyword, StringComparison.OrdinalIgnoreCase) ||
+            m.AwayTeam.Contains(keyword, StringComparison.OrdinalIgnoreCase) ||
+            m.Location.Contains(keyword, StringComparison.OrdinalIgnoreCase)
+        ).ToList();
+    }
+
+    public void DisplayAllMatches()
+    {
+        foreach (var match in matches)
+        {
+            Console.WriteLine($"\nMatch ID:- {match.MatchId}, \nSport:- {match.Sport}, \nDate:- {match.MatchDateTime}, \nLocation:- {match.Location}, \nTeams:- {match.HomeTeam} V/S {match.AwayTeam}, \nScores:- {match.HomeTeamScore} - {match.AwayTeamScore}");
+        }
+    }
+
+    public void DisplayMatchDetails(int matchId)
+    {
+        var match = matches.Find(m => m.MatchId == matchId);
+        if (match != null)
+        {
+            Console.WriteLine($"\nMatch ID:- {match.MatchId}, \nSport:- {match.Sport}, \nDate:- {match.MatchDateTime}, \nLocation:- {match.Location}, \nTeams:- {match.HomeTeam} V/S {match.AwayTeam}, \nScores:- {match.HomeTeamScore} - {match.AwayTeamScore}");
+        }
+        else
+        {
+            Console.WriteLine("Match Not Found");
+        }
+    }
+
+    public void UpdateMatchScore(int matchId, int homeScore, int awayScore)
+    {
+        var match = matches.Find(m => m.MatchId == matchId);
+        if (match != null)
+        {
+            match.HomeTeamScore = homeScore;
+            match.AwayTeamScore = awayScore;
+            Console.WriteLine("Scores Updated Successfully");
+        }
+        else
+        {
+            Console.WriteLine("Match Not Found");
+        }
+    }
+
+    public void RemoveMatch(int matchId)
+    {
+        var match = matches.Find(m => m.MatchId == matchId);
+        if (match != null)
+        {
+            matches.Remove(match);
+            Console.WriteLine("Match Removed Successfully");
+        }
+        else
+        {
+            Console.WriteLine("Match Not Found");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿namespace Match_Details_Management_System
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            MatchManagement matchManagement = new MatchManagement();
+
+            // Add at least 6 MatchDetails objects
+            matchManagement.AddMatch(new MatchDetails { MatchId = 1, Sport = "Soccer", MatchDateTime = DateTime.Parse("2023-09-07 14:00"), Location = "Stadium A", HomeTeam = "Team A", AwayTeam = "Team B", HomeTeamScore = 0, AwayTeamScore = 0 });
+            // Add more matches here...
+
+            bool continueRunning = true;
+
+            while (continueRunning)
+            {
+                Console.WriteLine("\n------------------------ MENU ---------------------");
+
+                Console.WriteLine("");
+
+                Console.WriteLine(" 1 -> To Display All Matches which is Stored");
+                Console.WriteLine(" 2 -> To Display Match Details");
+                Console.WriteLine(" 3 -> To Update Match Score");
+                Console.WriteLine(" 4 -> To Remove Match");
+                Console.WriteLine(" 5 -> To Sort Matches");
+                Console.WriteLine(" 6 -> To Filter Matches");
+                Console.WriteLine(" 7 -> To Calculate Statistics");
+                Console.WriteLine(" 8 -> To Search Matches");
+                Console.WriteLine(" 0 -> To Exit");
+
+                Console.WriteLine("");
+                // Console.WriteLin(\n);
+
+                int choice;
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            matchManagement.DisplayAllMatches();
+                            break;
+                        case 2:
+                            Console.Write("Enter Match ID to Display:- ");
+                            int matchId = int.Parse(Console.ReadLine());
+                            matchManagement.DisplayMatchDetails(matchId);
+                            break;
+                        case 3:
+                            Console.Write("Enter Match ID:- ");
+                            int id = int.Parse(Console.ReadLine());
+                            Console.Write("Enter Home Team Score:- ");
+                            int homeScore = int.Parse(Console.ReadLine());
+                            Console.Write("Enter Away Team Score:- ");
+                            int awayScore = int.Parse(Console.ReadLine());
+                            matchManagement.UpdateMatchScore(id, homeScore, awayScore);
+                            break;
+                        case 4:
+                            Console.Write("Enter Match ID:- ");
+                            int removeId = int.Parse(Console.ReadLine());
+                            matchManagement.RemoveMatch(removeId);
+                            break;
+                        case 5:
+                            Console.WriteLine("Sort By -> [ Date / Sport / Location] ");
+                            string criteria = Console.ReadLine();
+                            Console.WriteLine("Ascending? [ True / False ] ");
+                            bool ascending = bool.Parse(Console.ReadLine());
+                            matchManagement.SortMatches(criteria, ascending);
+                            matchManagement.DisplayAllMatches();
+                            break;
+                        case 6:
+                            Console.WriteLine("Filter By -> [ Sport / Location / Date Range] ");
+                            string filterCriteria = Console.ReadLine();
+                            Console.WriteLine("Enter value: ");
+                            string value = Console.ReadLine();
+                            List<MatchDetails> filteredMatches = matchManagement.FilterMatches(filterCriteria, value);
+                            Console.WriteLine("\nFiltered Matches:");
+                            foreach (var match in filteredMatches)
+                            {
+                                Console.WriteLine($"\nMatch ID:- {match.MatchId}, \nSport:- {match.Sport}, \nDate:- {match.MatchDateTime}, \nLocation:- {match.Location}, \nTeams:- {match.HomeTeam} V/S {match.AwayTeam}, \nScores:- {match.HomeTeamScore} - {match.AwayTeamScore}");
+                            }
+                            break;
+                        case 7:
+                            Console.WriteLine("Calculate statistics for: [ Average Score / Highest Score / Lowest Score]");
+                            string statisticsCriteria = Console.ReadLine();
+                            matchManagement.CalculateStatistics(statisticsCriteria);
+                            break;
+                        case 8:
+                            Console.WriteLine("Search for: ");
+                            string keyword = Console.ReadLine();
+                            List<MatchDetails> searchedMatches = matchManagement.SearchMatches(keyword);
+                            Console.WriteLine("\nSearched Matches:");
+                            foreach (var match in searchedMatches)
+                            {
+                                Console.WriteLine($"\nMatch ID:- {match.MatchId}, \nSport:- {match.Sport}, \nDate:- {match.MatchDateTime}, \nLocation:- {match.Location}, \nTeams:- {match.HomeTeam} V/S {match.AwayTeam}, \nScores:- {match.HomeTeamScore} - {match.AwayTeamScore}");
+                            }
+                            break;
+                        case 0:
+                            continueRunning = false;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Choice");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+        }
+    }
+}
